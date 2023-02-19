589d95eaa1e84def7ed0b0b09a17e4c01f9aae76
marianakemmerich <123078429+marianakemmerich@users.noreply.github.com>
2023-02-19 20:54:14 -0300

    update1

---
+++ lottery.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.17;
+
+contract Lottery{
+    address public manager;
+    address[] public players;
+
+    function Lottery() public {
+        manager = msg.sender;
+    }
+    function enter() public payable {
+        require(msg.value > .01 ether);
+
+        players.push(msg.sender);
+    }
+    function random() private view returns (uint) {
+        return uint(keccak256(block.difficulty, now, players));
+    }
+    function pickWinner() public restricted {
+        uint index = random() % players.length;
+        players[index].transfer(this.balance);
+        players = new address[](0);     
+    }
+    modifier restricted() {
+        // if the name of the function modifier is added to any other function inside of
+        // the contract, the solidity compiler will take all the code out of the function
+        // and stick it in where the underscore is
+        require(msg.sender == manager);
+        _;
+    }
+    function getPlayers() public view returns (address[]) {
+        return players;
+    }
+}
