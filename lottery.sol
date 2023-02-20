d71497469118fa0984163489295720b425b6d964
marianakemmerich <123078429+marianakemmerich@users.noreply.github.com>
2023-02-20 09:11:32 -0300

    update2 - comments

--- lottery.sol
+++ lottery.sol
@@ -7,7 +7,7 @@ contract Lottery{
     function Lottery() public {
         manager = msg.sender;
     }
-    function enter() public payable {
+    function enter() public payable { //enters a player into the contract
         require(msg.value > .01 ether);
 
         players.push(msg.sender);
@@ -15,15 +15,15 @@ contract Lottery{
     function random() private view returns (uint) {
         return uint(keccak256(block.difficulty, now, players));
     }
-    function pickWinner() public restricted {
+    function pickWinner() public restricted { // picks a winner
         uint index = random() % players.length;
-        players[index].transfer(this.balance);
-        players = new address[](0);     
+        players[index].transfer(this.balance); // verifies if the winner received the amount of money
+        players = new address[](0); // resets and empties out the player's array
     }
     modifier restricted() {
-        // if the name of the function modifier is added to any other function inside of
-        // the contract, the solidity compiler will take all the code out of the function
-        // and stick it in where the underscore is
+        //if the name of the function modifier is added to any other function inside of
+        //the contract, the solidity compiler will take all the code out of the function
+        //and stick it in where the underscore is
         require(msg.sender == manager);
         _;
     }
